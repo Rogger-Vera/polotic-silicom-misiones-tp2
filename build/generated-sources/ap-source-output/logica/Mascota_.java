3dafdaf0221c7172d6d7c8e9278ca36922d3a528
Rogger Vera <veraluiss23@gmail.com>
2021-11-18 04:10:15 -0300

    Subiendo archivo

---
+++ build/generated-sources/ap-source-output/logica/Mascota_.java
@@ -0,0 +1,21 @@
+package logica;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2021-11-16T19:50:18", comments="EclipseLink-2.7.7.v20200504-rNA")
+@StaticMetamodel(Mascota.class)
+public class Mascota_ { 
+
+    public static volatile SingularAttribute<Mascota, Integer> num_cliente;
+    public static volatile SingularAttribute<Mascota, String> nombre_perro;
+    public static volatile SingularAttribute<Mascota, String> raza;
+    public static volatile SingularAttribute<Mascota, String> nombre_duenio;
+    public static volatile SingularAttribute<Mascota, String> color;
+    public static volatile SingularAttribute<Mascota, Boolean> alergico;
+    public static volatile SingularAttribute<Mascota, String> observaciones;
+    public static volatile SingularAttribute<Mascota, Boolean> atencion_especial;
+    public static volatile SingularAttribute<Mascota, String> celular_duenio;
+
+}
